efb345510e977402085de86730cf9ebf09caf251
Rafael Langoni <langoninaves@gmail.com>
2022-08-11 16:40:50 -0300

    dia 2

---
+++ javascript/dia2.js
@@ -0,0 +1,12 @@
+// Write a function that takes two values, say a and b, as arguments
+// Return true if the two values are equal and of the same type
+function myFunction(a, b) {
+  if (a === b) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+}
+
+myFunction(2, "2");
+myFunction(2, 2);
