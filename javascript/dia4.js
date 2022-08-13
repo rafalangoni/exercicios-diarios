40beb49aa217c779fccf96c33fdb88a068f94a58
Rafael Langoni <langoninaves@gmail.com>
2022-08-13 14:58:23 -0300

    dia 4 - metodo slice de string

---
+++ javascript/dia4.js
@@ -0,0 +1,8 @@
+// Write a function that takes a string (a) as argument
+// Remove the first 3 characters of a
+// Return the result
+function retornaString(string) {
+    return string.slice(3);
+}
+
+console.log(retornaString("teste"))
