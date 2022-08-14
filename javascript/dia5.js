ff004de9846e84d88fde3f15b6556a5fd70bb5ea
Rafael Langoni <langoninaves@gmail.com>
2022-08-14 14:59:00 -0300

    slice de posicoes com array

--- javascript/dia5.js
+++ javascript/dia5.js
@@ -1,8 +1,20 @@
 // Write a function that takes a string (a) and a number (n) as argument
 // Return the nth character of 'a'
 function retornaPosicaoCaractere(palavra, posicao) {
-    return palavra[posicao];
+  return palavra[posicao];
 }
 
-console.log(retornaPosicaoCaractere("Rafael", 5))
+console.log(retornaPosicaoCaractere("Rafael", 5));
 
+/** */
+
+// Write a function that takes an array (a) as argument
+// Extract the first 3 elements of a
+// Return the resulting array
+function retorna3PrimeirasPosicoes(array) {
+  return array.slice(0,3);
+}
+
+const teste = [1,2,3,4,5]
+
+console.log(retorna3PrimeirasPosicoes(teste))
