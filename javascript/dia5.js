22c2d07338656b7da5f259fac461461e9d7bdcd7
Rafael Langoni <langoninaves@gmail.com>
2022-08-14 14:55:23 -0300

    dia5 - retorna posicao do caractere de uma string

---
+++ javascript/dia5.js
@@ -0,0 +1,8 @@
+// Write a function that takes a string (a) and a number (n) as argument
+// Return the nth character of 'a'
+function retornaPosicaoCaractere(palavra, posicao) {
+    return palavra[posicao];
+}
+
+console.log(retornaPosicaoCaractere("Rafael", 5))
+
