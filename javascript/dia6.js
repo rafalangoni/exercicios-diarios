987ec3c4f2d263b6116361c60b274f46f6293bd1
Rafael Langoni <langoninaves@gmail.com>
2022-08-15 16:21:11 -0300

    dia6 - retorna tamanho array

---
+++ javascript/dia6.js
@@ -0,0 +1,10 @@
+// Write a function that takes an array (a) as argument
+// Return the number of elements in a
+
+function retornaTamanhoArray(array){
+    return array.length;
+}
+
+const teste = [1,2,3,4,5]
+
+console.log(retornaTamanhoArray(teste));
