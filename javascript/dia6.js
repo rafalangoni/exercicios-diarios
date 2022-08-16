86470570164cd8e623f2aede897d45fe01b1c458
Rafael Langoni <39811730+rafalangoni@users.noreply.github.com>
2022-08-16 17:24:53 -0300

    Teste

--- javascript/dia6.js
+++ javascript/dia6.js
@@ -2,9 +2,9 @@
 // Return the number of elements in a
 
 function retornaTamanhoArray(array){
-    return array.length;
+    return array.length
 }
 
 const teste = [1,2,3,4,5]
 
-console.log(retornaTamanhoArray(teste));
+console.log(retornaTamanhoArray(teste))
