82096ca71099b68bb9126d157cd6f7ba1dcfe78e
Rafael Langoni <langoninaves@gmail.com>
2022-08-17 16:55:27 -0300

    dia7 - retorna chave de objeto

---
+++ javascript/dia7.js
@@ -0,0 +1,11 @@
+// Write a function that takes a string as argument
+// Create an object that has a property with key 'key' and a value equal to the string
+// Return the object
+
+function criaChaveObjeto(chave){
+    return {
+        key: chave
+    }
+}
+
+console.log(criaChaveObjeto("zé"))
