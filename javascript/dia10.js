5ca5c38179cff6279e6fa044cde3a2edd2ce3657
Rafael Langoni <langoninaves@gmail.com>
2022-08-20 19:21:50 -0300

    modificando metodo

--- javascript/dia10.js
+++ javascript/dia10.js
@@ -3,21 +3,23 @@
 // Tipp: you might want to use the square brackets property accessor
 
 const teste = {
-    prop2: "prop1",
-    nome: "nome1"
-}
+  "prop-2": "prop1",
+  nome: "nome1",
+};
+
 const teste2 = {
-    prop: "prop1",
-    nome: "nome1"
-}
+  "prop-2": "prop1",
+  nome: "nome1",
+};
 
+teste["prop-2"];
 
-function retornaProp2(objeto){
-    if(objeto.prop2 != null){
-        console.log("tem prop2")
-    }else{
-        console.log("não tem prop2")
-    }
+function retornaProp2(objeto) {
+  if (objeto["prop-2"] != null) {
+    console.log("tem prop2");
+  } else {
+    console.log("não tem prop2");
+  }
 }
-retornaProp2(teste)
-retornaProp2(teste2)
+retornaProp2(teste);
+retornaProp2(teste2);
