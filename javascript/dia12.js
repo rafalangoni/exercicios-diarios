44bf4c5065474547de6aac423e60f815595c222d
Rafael Langoni <langoninaves@gmail.com>
2022-08-22 16:33:48 -0300

    dia12-retorna posicao do array

---
+++ javascript/dia12.js
@@ -0,0 +1,11 @@
+// Write a function that takes an array (a) and a value (n) as argument
+// Return the nth element of 'a'
+
+const teste = [1,2,3,4,5]
+
+function posicaoDoArray(array,posicao){
+    return array[posicao];
+}
+
+console.log(posicaoDoArray(teste,2))
+
