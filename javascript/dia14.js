4f26e7bef47883d029f099a932fd2b6c900f8692
Rafael Langoni <langoninaves@gmail.com>
2022-09-09 17:10:54 -0300

    funcao par ou impar

---
+++ javascript/dia14.js
@@ -0,0 +1,12 @@
+// Write a function that takes a number as argument
+// If the number is even, return true
+// Otherwise, return false
+
+const parOuImpar = (numero) =>{
+    if(numero % 2 == 0){
+        return true;
+    }
+    return false;
+}
+
+console.log(parOuImpar(2))
