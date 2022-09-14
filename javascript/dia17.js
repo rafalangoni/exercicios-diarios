ce63b0459a3cc6769fab50a7a7e557b59933e596
Rafael Langoni <langoninaves@gmail.com>
2022-09-14 16:47:19 -0300

    funcao posicao string

--- javascript/dia17.js
+++ javascript/dia17.js
@@ -2,9 +2,14 @@
 // Extract the first half a
 // Return the result
 
-function extraiMetadeString(texto){
-    return texto.slice(0, texto.length/2)
-    
+function extraiMetadeString(texto) {
+  return texto.slice(0, texto.length / 2);
 }
 
-console.log(extraiMetadeString("Raspfiuqwi1bwiuecfael"))
+console.log(extraiMetadeString("Raspfiuqwi1bwiuecfael"));
+
+function myFunction(a, n) {
+  return a[n];
+}
+
+console.log(myFunction('Rafael',2))
