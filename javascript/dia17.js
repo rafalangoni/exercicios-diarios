026f058f17d6aaa345064707a6a37fa18d820686
Rafael Langoni <langoninaves@gmail.com>
2022-09-14 16:45:09 -0300

    dia17 - retorna metade de uma string

---
+++ javascript/dia17.js
@@ -0,0 +1,10 @@
+// Write a function that takes a string (a) as argument
+// Extract the first half a
+// Return the result
+
+function extraiMetadeString(texto){
+    return texto.slice(0, texto.length/2)
+    
+}
+
+console.log(extraiMetadeString("Raspfiuqwi1bwiuecfael"))
