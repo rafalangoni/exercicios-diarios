03dcbde740ea3d0de457ffc0524863b549d2ac5c
Rafael Langoni <langoninaves@gmail.com>
2022-09-23 16:54:50 -0300

    dia20 - retorna primeiras posicoes

---
+++ javascript/dia20.js
@@ -0,0 +1,12 @@
+// Write a function that takes a string (a) as argument
+// Get the first 3 characters of a
+// Return the result
+
+const nome = 'Rafael'
+
+const retorna3PrimeirasPosicoes = (texto) =>{
+    return texto.slice(0,3)
+}
+
+console.log(retorna3PrimeirasPosicoes(nome))
+
